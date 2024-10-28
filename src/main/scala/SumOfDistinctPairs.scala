4e0b00cc0879e80f4a7edbe0c9f2e29cd4d029d3
Reveim <piven_2001@mail.ru>
2024-10-28 11:46:46 +0300

    Update

--- src/main/scala/SumOfDistinctPairs.scala
+++ src/main/scala/SumOfDistinctPairs.scala
@@ -1,30 +1,28 @@
-class SumOfDistinctPairs {
+import scala.util.Random
 
-  import scala.util.Random
+object SumOfDistinctPairs extends App {
+  //Число меньше которого пары
+  val N = 10
+  //Кол-во элемнтов в списке
+  val numElements = 4
 
-  object SumOfDistinctPairs extends App {
-    //Число меньше которого пары
-    val N = 10
-    //Кол-во элемнтов в списке
-    val numElements = 4
+  //Генерация списока случайных чисел меньше N
+  val randomNumbers: List[Int] = List.fill(numElements)(Random.nextInt(N - 1) + 1)
 
-    //Генерация списока случайных чисел меньше N
-    val randomNumbers: List[Int] = List.fill(numElements)(Random.nextInt(N - 1) + 1)
+  println(s"Список чисел меньше $N: $randomNumbers")
 
-    println(s"Список чисел меньше $N: $randomNumbers")
+  //Генерерация пар неодиноковых чисел
+  val pairs: List[(Int, Int)] = for {
+    x <- randomNumbers
+    y <- randomNumbers
+    if x != y
+  } yield (x, y)
 
-    //Генерерация пар неодиноковых чисел
-    val pairs: List[(Int, Int)] = for {
-      x <- randomNumbers
-      y <- randomNumbers
-      if x != y
-    } yield (x, y)
+  println(s"Пары неодинаковых чисел: $pairs")
 
-    println(s"Пары неодинаковых чисел: $pairs")
+  val sumOfPairs: Int = pairs.map { case (a, b) => a + b }.sum
 
-    val sumOfPairs: Int = pairs.map { case (a, b) => a + b }.sum
-
-    println(s"Сумма всех пар неодинаковых чисел меньше $N: $sumOfPairs")
-  }
-
+  println(s"Сумма всех пар неодинаковых чисел меньше $N: $sumOfPairs")
 }
+
+
